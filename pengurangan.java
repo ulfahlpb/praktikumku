9719f887b332eb92ba913e0b101a4f64db0ac36a
ulfahlpb <ulfahnurulazhar22@gmil.com>
2014-09-23 12:00:24 +0700

    akku

---
+++ pengurangan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class pengurangan{
+public static void main(String[] args)
+{
+	Scanner in = new Scanner (System.in);
+	int x, y, z;
+	System.out.print("input nilai pertama = ");
+	x =in.nextInt();
+	System.out.print("input nilai kedua = ");
+	y =in.nextInt();
+	z = x - y;
+	System.out.println("Hasil= " + z);
+	}
+}
+
